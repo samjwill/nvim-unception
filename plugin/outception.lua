9f2ffad35cf470e7b4d2296d631b5c36dc1ca488
SamuelWilliams256 <25990267+SamuelWilliams256@users.noreply.github.com>
2022-05-21 22:21:40 -0400

    Initial commit.

---
+++ plugin/outception.lua
@@ -0,0 +1,30 @@
+function exists(filename)
+   local ok, message, err_code = os.rename(filename, filename)
+   if not ok then
+      if err_code == 13 then
+         --file inaccessible, but was found
+         ok = true
+      end
+   end
+   return ok
+end
+
+local expected_pipe_name = "/tmp/nvim.pipe"
+
+if exists(expected_pipe_name) then
+    args = vim.call("argv")
+
+    local arg_str = ""
+    for index, iter in pairs(args) do
+        arg_str = arg_str.." "..iter
+    end
+
+    if (#args > 0) then
+        os.execute("\\nvim --server "..expected_pipe_name.." --remote-send \"<C-\\><C-N>:argedit "..arg_str.."<CR>\"")
+    else
+        os.execute("\\nvim --server "..expected_pipe_name.." --remote-send \"<C-\\><C-N>:enew<CR>\"")
+    end
+    vim.cmd("quit")
+else
+    vim.call("serverstart", expected_pipe_name)
+end
