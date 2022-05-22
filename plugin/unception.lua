cfb6333c55dd2d11effd2d940c251c08f5897669
samjwill <25990267+samjwill@users.noreply.github.com>
2022-05-22 19:45:18 -0400

    Cleaned up code a bit more.

--- plugin/unception.lua
+++ plugin/unception.lua
@@ -16,39 +16,39 @@ local function exists(filename)
 end
 
 local function build_command(arg_str, number_of_args, server_address)
-    local execute_command = "\\nvim --server "..server_address.." --remote-send "
+    local cmd_to_execute = "\\nvim --server "..server_address.." --remote-send "
 
     -- start command to be run by server
-    execute_command = execute_command.."\""
+    cmd_to_execute = cmd_to_execute.."\""
 
     -- exit terminal-insert mode
-    execute_command = execute_command.."<C-\\><C-N>"
+    cmd_to_execute = cmd_to_execute.."<C-\\><C-N>"
 
     -- log buffer number so that we can delete it later. We don't want a ton of
     -- running terminal buffers in the background when we switch to a new nvim buffer.
-    execute_command = execute_command..":silent let g:unception_tmp_bufnr = bufnr() | "
+    cmd_to_execute = cmd_to_execute..":silent let g:unception_tmp_bufnr = bufnr() | "
 
     -- If there aren't arguments, we just want a new, empty buffer, but if
     -- there are, append them to the host Neovim session's arguments list.
     if (number_of_args > 0) then
-        execute_command = execute_command.."silent argedit "..arg_str.." | "
+        cmd_to_execute = cmd_to_execute.."silent argedit "..arg_str.." | "
     else
-        execute_command = execute_command.."silent enew | "
+        cmd_to_execute = cmd_to_execute.."silent enew | "
     end
 
     -- remove the old terminal buffer
-    execute_command = execute_command.."silent execute 'bdelete! ' . g:unception_tmp_bufnr | "
+    cmd_to_execute = cmd_to_execute.."silent execute 'bdelete! ' . g:unception_tmp_bufnr | "
 
     -- remove temporary variable
-    execute_command = execute_command.."silent unlet g:unception_tmp_bufnr | "
+    cmd_to_execute = cmd_to_execute.."silent unlet g:unception_tmp_bufnr | "
 
     -- remove command from history and send it
-    execute_command = execute_command.."call histdel(':', -1)<CR>"
+    cmd_to_execute = cmd_to_execute.."call histdel(':', -1)<CR>"
 
     -- flavor text :)
-    execute_command = execute_command..":echo 'Unception!' | call histdel(':', -1)<CR>\""
+    cmd_to_execute = cmd_to_execute..":echo 'Unception!' | call histdel(':', -1)<CR>\""
 
-    return execute_command
+    return cmd_to_execute
 end
 
 local username = os.getenv("USER")
@@ -68,9 +68,9 @@ if exists(expected_pipe_name) then
         arg_str = arg_str.." "..iter
     end
 
-    local execute_command = build_command(arg_str, #args, expected_pipe_name)
+    local cmd_to_execute = build_command(arg_str, #args, expected_pipe_name)
 
-    os.execute(execute_command)
+    os.execute(cmd_to_execute)
 
     -- Our work here is done. Kill the nvim session that would have started otherwise.
     vim.cmd("quit")
