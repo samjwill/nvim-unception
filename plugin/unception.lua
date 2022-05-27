095d7eecf506daead09328cbdab5b8b17d0eacce
Samuel Williams <25990267+samjwill@users.noreply.github.com>
2022-05-27 18:57:03 -0400

    Update unception.lua

--- plugin/unception.lua
+++ plugin/unception.lua
@@ -47,9 +47,9 @@ local function build_command(arg_str, number_of_args, server_address)
 
     -- This is kind of stupid, but basically I've noticed that some
     -- plugins, like Treesitter, don't appear to properly trigger when
-    -- receiving a server command with argedit. Basically just re-edit the
-    -- same file here to get it to trigger, since doing so doesn't hurt
-    -- anything.
+    -- receiving a server command with argedit. Basically, just re-edit the
+    -- same file here to get stuff like Treesitter's syntax highlighting to
+    -- trigger, since doing so doesn't hurt anything.
     cmd_to_execute = cmd_to_execute.."e | "
 
     -- remove command from history and send it
