a80531a62044a1278489244ce5d9952c0c3233bf
samjwill <25990267+samjwill@users.noreply.github.com>
2022-06-05 12:21:36 -0400

    Fixed typo.

--- lua/init_config_defaults.lua
+++ lua/init_config_defaults.lua
@@ -2,7 +2,7 @@ if(vim.g.unception_delete_replaced_buffer == nil) then
     vim.g.unception_delete_replaced_buffer = false
 end
 
-if(vim.g.open_buffers_in_new_tab == nil) then
+if(vim.g.unception_open_buffer_in_new_tab == nil) then
     vim.g.unception_open_buffer_in_new_tab = false
 end
 
