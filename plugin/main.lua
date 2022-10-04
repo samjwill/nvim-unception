7479e2a65daa7c95bb19e040dcc22fa4bfc6ae2c
Samuel Williams <25990267+samjwill@users.noreply.github.com>
2022-10-04 10:14:13 -0400

    Formatting fix.

--- plugin/main.lua
+++ plugin/main.lua
@@ -4,7 +4,7 @@ if (vim.g.unception_disable) then
     return
 end
 
-if 1 ~= vim.fn.has "nvim-0.7.0" then
+if (1 ~= vim.fn.has("nvim-0.7.0")) then
     vim.api.nvim_err_writeln "Unception requires Neovim 0.7 or later."
     return
 end
