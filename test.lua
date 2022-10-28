30ef916fffc49b3e90e89b7986ca06d80bf728e6
samjwill <25990267+samjwill@users.noreply.github.com>
2022-10-28 17:19:53 -0400

    Fleshed out idea more.

--- test.lua
+++ test.lua
@@ -16,3 +16,11 @@ echo rpcrequest(my_socket, "nvim_eval", "getpid()")
 -- ISSUE: We only want to block on the side of the client making the request.
 -- We do not want to block serverside. Ergo, this needs thought out some more,
 -- as the RPC functions are executed synchronously instead of asynchronously.
+--
+-- Updated idea: Have client open file normally, and then loop every .5 seconds
+-- asking host session if currently editing the .gitcommit file. Server returns
+-- true or false. Note: will need to test initial --remote function to ensure
+-- that it is syncronous; we need to ensure that host neovim actually has time
+-- to open the git commit file.
+--
+-- https://github.com/neovim/neovim/blob/0c537240df9ceaa9f9019d24d6d4dddea7744387/runtime/lua/vim/_editor.lua#L699-L706
