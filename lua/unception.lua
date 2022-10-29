5e8f63e9ae8c6355395b5780863957be0734961d
samjwill <25990267+samjwill@users.noreply.github.com>
2022-10-28 21:47:38 -0400

    Continued

--- lua/unception.lua
+++ lua/unception.lua
@@ -129,20 +129,16 @@ local filepath_to_check = ""
 function _G.handle_unloaded_buffer(unloaded_buffer_filepath)
     unloaded_buffer_filepath = get_absolute_filepath(unloaded_buffer_filepath)
 
+    if (unloaded_buffer_filepath == filepath_to_check) then
+        print("ITS A MATCH!")
+    end
+
     print(unloaded_buffer_filepath)
     print(filepath_to_check)
-    print(filepath_to_check)
-    print(filepath_to_check)
-    print(filepath_to_check)
-    print(unloaded_buffer_filepath)
-    print(unloaded_buffer_filepath)
-    print(unloaded_buffer_filepath)
-    print(unloaded_buffer_filepath)
-    print(unloaded_buffer_filepath)
 end
 
 function _G.tmp_unception_still_being_edited(filepath)
-    filepath_to_check = get_absolute_filepath(filepath)
+    filepath_to_check = filepath
     vim.api.nvim_create_autocmd("BufUnload",{ command = "lua handle_unloaded_buffer(vim.fn.expand('<afile>:p'))"})
     return filepath_to_check
 end
