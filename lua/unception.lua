d847f422264cb78bba368f6d59b1330ab630cf81
samjwill <25990267+samjwill@users.noreply.github.com>
2022-10-28 20:36:51 -0400

    Might have broken it?

--- lua/unception.lua
+++ lua/unception.lua
@@ -114,7 +114,7 @@ else
 
     if (vim.g.unception_block_while_editing) then
         local sock = vim.fn.sockconnect("pipe", existing_server_pipe_path, {rpc = true})
-        print(vim.fn.rpcrequest(sock, "nvim_exec_lua", "return tmp_unception_still_being_edited()", {}))
+        print(vim.fn.rpcrequest(sock, "nvim_exec_lua", "return tmp_unception_still_being_edited("..arg_str..")", {}))
     end
 
     -- Our work here is done. Kill the nvim session that would have started otherwise.
@@ -122,7 +122,7 @@ else
 end
 
 
-function _G.tmp_unception_still_being_edited()
-    return true
+function _G.tmp_unception_still_being_edited(filepath)
+    return filepath
 end
 
