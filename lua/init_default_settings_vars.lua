4871bc5a2a99dcaeb5f4d204777deb6e2360d3d2
samjwill <25990267+samjwill@users.noreply.github.com>
2022-10-30 19:35:05 -0400

    Updated comment.

--- lua/init_default_settings_vars.lua
+++ lua/init_default_settings_vars.lua
@@ -14,12 +14,13 @@ if (vim.g.unception_enable_flavor_text == nil) then
     vim.g.unception_enable_flavor_text = true
 end
 
-if(vim.g.unception_block_while_editing == nil) then
+if (vim.g.unception_block_while_editing == nil) then
     vim.g.unception_block_while_editing = false
 end
 
--- Can't allow buffer holding terminal to be deleted.
-if vim.g.unception_block_while_editing then
+-- Can't allow buffer holding terminal to be deleted. Also don't want the
+-- terminal buffer to get hidden, so open the buffer being edited in a new tab.
+if (vim.g.unception_block_while_editing) then
     vim.g.unception_open_buffer_in_new_tab = true
     vim.g.unception_delete_replaced_buffer = false
 end
