ffee962ef8b92084c97fed7765178be42e911be3
samjwill <25990267+samjwill@users.noreply.github.com>
2022-10-30 18:49:32 -0400

    Finally got it working!

--- lua/server_functions.lua
+++ lua/server_functions.lua
@@ -1,9 +1,10 @@
 local response_sock -- TODO: Unsure if I actually have to declare it here.
+local unception_bufunload_autocmd_id
 
 function _G.tmp_unception_still_being_edited(pipe_to_respond_on, filepath)
     filepath_to_check = filepath
     response_sock = vim.fn.sockconnect("pipe", pipe_to_respond_on, {rpc = true})
-    vim.api.nvim_create_autocmd("BufUnload",{ command = "lua handle_unloaded_buffer(vim.fn.expand('<afile>:p'))"})
+    unception_bufunload_autocmd_id = vim.api.nvim_create_autocmd("BufUnload",{ command = "lua handle_unloaded_buffer(vim.fn.expand('<afile>:p'))"})
     return filepath_to_check
 end
 
@@ -14,6 +15,9 @@ function _G.handle_unloaded_buffer(unloaded_buffer_filepath)
     if (unloaded_buffer_filepath == filepath_to_check) then
         print("ITS A MATCH!")
         print("Going to respond on: "..response_sock)
+        vim.api.nvim_del_autocmd(unception_bufunload_autocmd_id)
+        vim.fn.rpcnotify(response_sock, "nvim_exec_lua", "vim.cmd('quit')", {})
+
         --TODO: Use rpcnotify instead?
         --TODO: send notify out to client that its buffer was unloaded and that it can stop blocking
         --TODO: delete the autocmd
