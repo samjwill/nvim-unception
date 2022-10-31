77bf68be65155e577da6d1da55529947d7eba257
samjwill <25990267+samjwill@users.noreply.github.com>
2022-10-31 02:11:01 -0400

    Made a function non-global.

--- lua/server_callback.lua
+++ lua/server_callback.lua
@@ -3,15 +3,7 @@ require("common_functions")
 local response_sock
 local unception_bufunload_autocmd_id
 
--- TODO: Global necessary?
-function _G.unception_notify_when_done_editing(pipe_to_respond_on, filepath)
-    filepath_to_check = filepath
-    response_sock = vim.fn.sockconnect("pipe", pipe_to_respond_on, {rpc = true})
-    unception_bufunload_autocmd_id = vim.api.nvim_create_autocmd("BufUnload",{ command = "lua unception_handle_unloaded_buffer(vim.fn.expand('<afile>:p'))"})
-    return filepath_to_check
-end
-
-function _G.unception_handle_unloaded_buffer(unloaded_buffer_filepath)
+function unception_handle_unloaded_buffer(unloaded_buffer_filepath)
     unloaded_buffer_filepath = get_absolute_filepath(unloaded_buffer_filepath)
 
     if (unloaded_buffer_filepath == filepath_to_check) then
@@ -20,5 +12,12 @@ function _G.unception_handle_unloaded_buffer(unloaded_buffer_filepath)
         -- TODO: Find out if this is necessary.
         -- vim.fn.chanclose(response_sock)
     end
+end
+
+function _G.unception_notify_when_done_editing(pipe_to_respond_on, filepath)
+    filepath_to_check = filepath
+    response_sock = vim.fn.sockconnect("pipe", pipe_to_respond_on, {rpc = true})
+    unception_bufunload_autocmd_id = vim.api.nvim_create_autocmd("BufUnload",{ command = "lua unception_handle_unloaded_buffer(vim.fn.expand('<afile>:p'))"})
+    return filepath_to_check
 end
 
