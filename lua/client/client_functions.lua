2cf4ae39aeb599f50f7b4c5fc2175c31e3a5ed55
samjwill <25990267+samjwill@users.noreply.github.com>
2022-11-01 00:06:07 -0400

    Got it working again.

--- lua/client/client_functions.lua
+++ lua/client/client_functions.lua
@@ -1,15 +1,7 @@
 function build_command(arg_str, number_of_args, server_address)
-    local cmd_to_execute = "\\nvim --server "..server_address.." --remote-send "
-
-    -- start command to be run by server
-    cmd_to_execute = cmd_to_execute.."\""
-
-    -- exit terminal-insert mode
-    cmd_to_execute = cmd_to_execute.."<C-\\><C-N>"
-
     -- log buffer number so that we can delete it later. We don't want a ton of
     -- running terminal buffers in the background when we switch to a new nvim buffer.
-    cmd_to_execute = cmd_to_execute..":silent let g:unception_tmp_bufnr = bufnr() | "
+    cmd_to_execute = "silent let g:unception_tmp_bufnr = bufnr() | "
 
     if vim.g.unception_open_buffer_in_new_tab then
         cmd_to_execute = cmd_to_execute.."silent tabnew | "
@@ -18,9 +10,12 @@ function build_command(arg_str, number_of_args, server_address)
     -- If there aren't arguments, we just want a new, empty buffer, but if
     -- there are, append them to the host Neovim session's arguments list.
     if (number_of_args > 0) then
+        -- Apparently need to escape backslashes again.
+        local escaped_arg_list = string.gsub(arg_str, "\\", "\\\\")
+
         -- Had some issues when using argedit. Explicitly calling these
         -- separately appears to work though.
-        cmd_to_execute = cmd_to_execute.."silent 0argadd "..arg_str.." | "
+        cmd_to_execute = cmd_to_execute.."silent 0argadd "..escaped_arg_list.." | "
         cmd_to_execute = cmd_to_execute.."silent argument 1 | "
 
         -- This is kind of stupid, but basically, I've noticed that some
@@ -46,14 +41,11 @@ function build_command(arg_str, number_of_args, server_address)
     cmd_to_execute = cmd_to_execute.."silent unlet g:unception_tmp_bufnr | "
 
     -- remove command from history and enter it
-    cmd_to_execute = cmd_to_execute.."call histdel(':', -1)<CR>"
+    cmd_to_execute = cmd_to_execute.."call histdel(':', -1)"
 
     if (vim.g.unception_enable_flavor_text) then
-        cmd_to_execute = cmd_to_execute..":echo 'Unception prevented inception!' | call histdel(':', -1)<CR>"
+        cmd_to_execute = cmd_to_execute.." | echo 'Unception prevented inception!' | call histdel(':', -1)"
     end
-
-    -- end command to be run by server
-    cmd_to_execute = cmd_to_execute.."\""
 
     return cmd_to_execute
 end
