65cd9dba1def12b332448ae63857963fa35124ec
samjwill <25990267+samjwill@users.noreply.github.com>
2022-11-02 00:29:31 -0400

    Tweaked if statement.

--- lua/unception.lua
+++ lua/unception.lua
@@ -1,9 +1,9 @@
 existing_server_pipe_path = os.getenv("NVIM_UNCEPTION_PIPE_PATH_HOST")
 in_terminal_buffer = (existing_server_pipe_path ~= nil)
 
-if not in_terminal_buffer then
-    require("server.server")
-else
+if in_terminal_buffer then
     require("client.client")
+else
+    require("server.server")
 end
 
