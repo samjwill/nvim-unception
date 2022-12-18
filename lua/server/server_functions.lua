6fc77c358098e2ab540fdf3d5f60b0e6d70682e3
samjwill <25990267+samjwill@users.noreply.github.com>
2022-12-18 18:40:16 -0500

    Fixed bug involving user using other terminals to open buffers mid-commit.

--- lua/server/server_functions.lua
+++ lua/server/server_functions.lua
@@ -4,23 +4,25 @@ local response_sock = nil
 local unception_quitpre_autocmd_id = nil
 local unception_bufunload_autocmd_id = nil
 local filepath_to_check = nil
-local id_of_replaced_buffer = nil
+local blocked_terminal_buffer_id = nil
+local last_replaced_buffer_id = nil
 
 function unblock_client_and_reset_state()
-    -- Remove the autocmds we made
+    -- Remove the autocmds we made.
     vim.api.nvim_del_autocmd(unception_quitpre_autocmd_id)
     vim.api.nvim_del_autocmd(unception_bufunload_autocmd_id)
 
-    -- Unblock client by killing editor session.
+    -- Unblock client by killing its editor session.
     vim.fn.rpcnotify(response_sock, "nvim_exec_lua", "vim.cmd('quit')", {})
     vim.fn.chanclose(response_sock)
 
-    -- Reset state-sensitive variables
+    -- Reset state-sensitive variables.
     response_sock = nil
     unception_quitpre_autocmd_id = nil
     unception_bufunload_autocmd_id = nil
     filepath_to_check = nil
-    id_of_replaced_buffer = nil
+    blocked_terminal_buffer_id = nil
+    last_replaced_buffer_id = nil
 end
 
 function unception_handle_bufunload(unloaded_buffer_filepath)
@@ -38,9 +40,9 @@ function unception_handle_quitpre(quitpre_buffer_filepath)
 
     if (quitpre_buffer_filepath == filepath_to_check) then
         -- If there was a replaced buffer, we should restore it to the same window.
-        if (id_of_replaced_buffer ~= nil) then
-            vim.cmd("split") -- Open a new window and switch focus to it
-            vim.cmd("buffer " .. id_of_replaced_buffer) -- Set the buffer for that window to the buffer that was replaced.
+        if (blocked_terminal_buffer_id ~= nil) then
+            vim.cmd("split") -- Open a new window and switch focus to it.
+            vim.cmd("buffer " .. blocked_terminal_buffer_id) -- Set the buffer for that window to the buffer that was replaced.
             vim.cmd("wincmd x") -- Navigate to previous (initial) window, and proceed with quitting.
         end
 
@@ -50,10 +52,11 @@ end
 
 function _G.unception_notify_when_done_editing(pipe_to_respond_on, filepath)
     filepath_to_check = filepath
+    blocked_terminal_buffer_id = last_replaced_buffer_id
     response_sock = vim.fn.sockconnect("pipe", pipe_to_respond_on, {rpc = true})
     unception_quitpre_autocmd_id = vim.api.nvim_create_autocmd("QuitPre",{ command = "lua unception_handle_quitpre(vim.fn.expand('<afile>:p'))"})
 
-    -- Create an autocmd for BufUnload as a failsafe should QuitPre not get triggered on the target buffer (e.g. if a user runs :bdelete)
+    -- Create an autocmd for BufUnload as a failsafe should QuitPre not get triggered on the target buffer (e.g. if a user runs :bdelete).
     unception_bufunload_autocmd_id = vim.api.nvim_create_autocmd("BufUnload",{ command = "lua unception_handle_bufunload(vim.fn.expand('<afile>:p'))"})
 end
 
@@ -72,9 +75,10 @@ function _G.unception_edit_files(file_args, num_files_in_list, open_in_new_tab, 
         vim.cmd("0argadd "..file_args)
 
         if (open_in_new_tab) then
+            last_replaced_buffer_id = nil
             vim.cmd("tab argument 1")
         else
-            id_of_replaced_buffer = vim.fn.bufnr()
+            last_replaced_buffer_id = vim.fn.bufnr()
             vim.cmd("argument 1")
         end
 
@@ -87,15 +91,16 @@ function _G.unception_edit_files(file_args, num_files_in_list, open_in_new_tab, 
         vim.cmd("edit")
     else
         if (open_in_new_tab) then
+            last_replaced_buffer_id = nil
             vim.cmd("tabnew")
         else
-            id_of_replaced_buffer = vim.fn.bufnr()
+            last_replaced_buffer_id = vim.fn.bufnr()
             vim.cmd("enew")
         end
     end
 
-    -- We don't want to delete the replaced buffer if there wasn't a replaced buffer
-    if (delete_replaced_buffer and not open_in_new_tab) then
+    -- We don't want to delete the replaced buffer if there wasn't a replaced buffer.
+    if (delete_replaced_buffer and last_replaced_buffer_id ~= nil) then
         if (vim.fn.len(vim.fn.win_findbuf(tmp_buf_number)) == 0) then
             pcall(vim.cmd, "bdelete! "..tmp_buf_number) -- Use pcall so it doesn't complain if it fails to delete the buffer.
         end
