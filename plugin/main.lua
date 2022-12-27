00d6f0d0d634cf3181a977796af1b77fe29a010f
samjwill <25990267+samjwill@users.noreply.github.com>
2022-12-27 18:21:19 -0500

    Added headless/embed check

--- plugin/main.lua
+++ plugin/main.lua
@@ -1,13 +1,52 @@
-require("init_default_settings_vars")
+-------------------------------------------------------------------------------
+-- Initialize all expected variables
+-------------------------------------------------------------------------------
+if(vim.g.unception_delete_replaced_buffer == nil) then
+    vim.g.unception_delete_replaced_buffer = false
+end
 
+if(vim.g.unception_open_buffer_in_new_tab == nil) then
+    vim.g.unception_open_buffer_in_new_tab = false
+end
+
+if (vim.g.unception_enable_flavor_text == nil) then
+    vim.g.unception_enable_flavor_text = true
+end
+
+if (vim.g.unception_block_while_host_edits == nil) then
+    vim.g.unception_block_while_host_edits = false
+end
+
+-- Can't allow buffer holding terminal to be deleted.
+if (vim.g.unception_block_while_host_edits) then
+    vim.g.unception_delete_replaced_buffer = false
+end
+
+if (vim.g.unception_disable  == nil) then
+    vim.g.unception_disable = false
+end
+
+-- If in ":h --headless" or ":h --embed" mode, a UI is not desired, so don't
+-- run the plugin.
+if (0 == #vim.api.nvim_list_uis()) then
+    vim.g.unception_disable = true
+end
+
+-- Version check to ensure that necessary features are available.
+if (1 ~= vim.fn.has("nvim-0.7.0")) then
+    vim.api.nvim_err_writeln("Unception requires Neovim 0.7 or later.")
+    vim.g.unception_disable = true
+end
+
+-------------------------------------------------------------------------------
+-- Handle early exit if necessary
+-------------------------------------------------------------------------------
 if (vim.g.unception_disable) then
     return
 end
 
-if (1 ~= vim.fn.has("nvim-0.7.0")) then
-    vim.api.nvim_err_writeln("Unception requires Neovim 0.7 or later.")
-    return
-end
-
+-------------------------------------------------------------------------------
+-- We're good to go. Start the main plugin logic.
+-------------------------------------------------------------------------------
 require("unception")
 
