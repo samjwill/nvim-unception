8746e70ca00c72bb8861aaff87d58d5699cdf942
Samuel Williams <25990267+samjwill@users.noreply.github.com>
2023-03-10 15:09:12 -0500

    Prefix global lua vars with `unception`. (#67)
    * Update client.lua
    
    * Update server.lua
    
    * Update unception.lua

--- lua/server/server.lua
+++ lua/server/server.lua
@@ -1,5 +1,5 @@
 require("server.server_functions")
 
 local new_server_pipe_path = vim.call("serverstart")
-vim.call("setenv", pipe_path_host_env_var, new_server_pipe_path)
+vim.call("setenv", unception_pipe_path_host_env_var, new_server_pipe_path)
 
--- lua/unception.lua
+++ lua/unception.lua
@@ -1,6 +1,5 @@
-pipe_path_host_env_var = "NVIM_UNCEPTION_PIPE_PATH_HOST"
-existing_server_pipe_path = os.getenv(pipe_path_host_env_var)
-local in_terminal_buffer = (existing_server_pipe_path ~= nil)
+unception_pipe_path_host_env_var = "NVIM_UNCEPTION_PIPE_PATH_HOST"
+local in_terminal_buffer = (os.getenv(unception_pipe_path_host_env_var) ~= nil)
 
 if in_terminal_buffer then
     require("client.client")
