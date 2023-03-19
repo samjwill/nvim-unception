fbb503f99b2b12de9de6704586feb6ebae9e6de7
samjwill <25990267+samjwill@users.noreply.github.com>
2023-03-19 19:17:57 -0400

    Fixed issue where host session could unintentionally error out.

--- lua/unception.lua
+++ lua/unception.lua
@@ -1,5 +1,4 @@
-unception_pipe_path_host_env_var = "NVIM_UNCEPTION_PIPE_PATH_HOST"
-local in_terminal_buffer = (os.getenv(unception_pipe_path_host_env_var) ~= nil)
+local in_terminal_buffer = (os.getenv("NVIM") ~= nil)
 
 if in_terminal_buffer then
     require("client.client")
