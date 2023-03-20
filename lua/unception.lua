705a9fcf88fcfc4e61b5f2c2a07156e56b5b0400
samjwill <25990267+samjwill@users.noreply.github.com>
2023-03-20 09:34:47 -0400

    Reverted partial 8c8b933eaff4c22d9258c30b1b86a1a18ede2667

--- lua/unception.lua
+++ lua/unception.lua
@@ -1,4 +1,5 @@
-local in_terminal_buffer = (os.getenv("NVIM") ~= nil)
+unception_pipe_path_host_env_var = "NVIM_UNCEPTION_PIPE_PATH_HOST"
+local in_terminal_buffer = (os.getenv(unception_pipe_path_host_env_var) ~= nil)
 
 if in_terminal_buffer then
     require("client.client")
